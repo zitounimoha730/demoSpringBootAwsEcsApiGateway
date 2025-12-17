3f8aa91195442cc5ee154c0405e4a1493c0076a3
Mohamed Zitouni <zitounimoha730@gmail.com>
2025-12-17 15:05:07 +0000

    fix type check

--- demoBooksSpringBootApi/src/main/java/com/example/demobooksspringbootapi/application/rest/book/dto/BookDto.java
+++ demoBooksSpringBootApi/src/main/java/com/example/demobooksspringbootapi/application/rest/book/dto/BookDto.java
@@ -2,6 +2,7 @@ package com.example.demobooksspringbootapi.application.rest.book.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.With;
 
@@ -12,7 +13,7 @@ public record BookDto(@JsonProperty(access = JsonProperty.Access.READ_ONLY)
                       Long id,
                       @NotBlank(message = "Title is required!")
                       String title,
-                      @NotBlank(message = "Title is required!")
+                      @NotNull(message = "Title is required!")
                       @PositiveOrZero(message = "Price can't be negative!")
                       double price,
                       boolean active,
